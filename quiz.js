0c3c84d438b7738b5705a61c80f18fae85e7530b
yusufqasala <140950647+yusufqasala@users.noreply.github.com>
2024-12-29 18:39:23 +0200

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,19 +1,15 @@
+
 function checkAnswer() {
-  const correctAnswer = "4";
-
-  const userAnswer = document.querySelector('input[name="quiz"]:checked');
-
-  if (userAnswer) {
-    if (userAnswer.value === correctAnswer) {
-      document.getElementById("feedback").textContent = "Correct! Well done.";
+    const correctAnswer = "4";
+    const userAnswer = document.querySelector('input[name="quiz"]:checked');
+    if (userAnswer) {
+        if (userAnswer.value === correctAnswer) {
+            document.getElementById("feedback").textContent = "Correct! Well done.";
+        } else {
+            document.getElementById("feedback").textContent = "That's incorrect. Try again!";
+        }
     } else {
-      document.getElementById("feedback").textContent =
-        "That's incorrect. Try again!";
+        document.getElementById("feedback").textContent = "Please select an answer.";
     }
-  } else {
-    document.getElementById("feedback").textContent =
-      "Please select an answer.";
-  }
 }
-
 document.getElementById("submit-answer").addEventListener("click", checkAnswer);
